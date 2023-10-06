89937250420f5bc3c3d484873f0c93c473364194
Gabriel Koehler da Silva <gabrielks@weg.net>
2023-10-06 17:01:26 -0300

    V2

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,86 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue';
+
+let array = ref<Number[]>([])
+let numeroCasa = ref<Number>(49);
+let numeroParede = ref<Number[]>([])
+let persoonagem = 2;
+
+for (let index = 0; index < 60; index++) {
+  let casa: Number = index + 1;
+  array.value.push(casa);
+  if (casa != 1 && casa != 2 && casa != 3 && casa != 4 && casa != 5 && casa != 6 && casa != 7 && casa != 8 && casa != 9 && casa != 10 && casa != 11 && casa != 12 && casa != 13 && casa != 25 && casa != 37 && casa != 49) {
+    numeroParede.value.push(casa);
+    console.log(numeroParede);
+  }
+
+}
+
+function mudaCasa(): void {
+  if (event.key == 's') {
+
+    if (numeroCasa.value + 12 <= 60) {
+      numeroCasa.value += 12
+    }
+
+  }
+  if (event.key == 'w') {
+
+    if (numeroCasa.value - 12 > 0) {
+      numeroCasa.value -= 12
+    }
+
+  }
+  if (event.key == 'd') {
+    if (numeroCasa.value % 12 == 0) {
+
+    } else {
+      numeroCasa.value += 1
+    }
+  }
+
+  if (event.key == 'a') {
+    if ((numeroCasa.value + 11) % 12 == 0) {
+    } else {
+      numeroCasa.value -= 1
+    }
+  }
+}
+onMounted(() => {
+  window.addEventListener('keypress', mudaCasa);
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <section>
+    <div class="grid-full grid grid-cols-12 gap-4">
+      <div class="box" v-for="numero of array">
+          <div v-if="numeroCasa == numero">
+            {{ persoonagem }}
+          </div>
+        <div  v-for="numeroParedeA of numeroParede">
+        <div class="bg-slate-600 parede" v-if="numeroParedeA == numero">
+          {{ numeroParedeA }}
+        </div>
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.grid-full {
+  @apply w-screen h-screen;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.box {
+  @apply border-sky-500 border-2 w-full h-full;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.parede {
+  width: 100%;
+  height: 100%;
 }
 </style>
